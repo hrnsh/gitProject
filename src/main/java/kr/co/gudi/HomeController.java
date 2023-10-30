97a796db2e13d2e2826a9e9aeef96e75d87dcc0d
jsw <remaintime10@gmail.com>
2023-10-30 16:13:41 +0900

    pushtem

--- src/main/java/kr/co/gudi/HomeController.java
+++ src/main/java/kr/co/gudi/HomeController.java
@@ -25,15 +25,14 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
 		model.addAttribute("msg", "hello_world" );
 		
 		return "home";
+	}
+	
+	@RequestMapping(value = "/list")
+	public String list(Model model) {
+		return"list";
 	}
 	
 }
